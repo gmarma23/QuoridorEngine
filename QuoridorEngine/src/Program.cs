6fe50ecfcd69ca24d05bb6fe4775c54989981c77
gmarma23 <gmarma99@gmail.com>
2022-11-01 21:23:28 +0200

    Added print msg

--- QuoridorEngine/src/Program.cs
+++ QuoridorEngine/src/Program.cs
@@ -12,7 +12,7 @@ namespace QuoridorEngine
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-            // sireluielgure
+            Console.WriteLine("Test");
         }
     }
 }
