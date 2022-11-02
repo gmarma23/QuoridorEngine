6f69a35dcd34ed5a7d6cc6abb63df984b87cff17
gmarma23 <gmarma99@gmail.com>
2022-11-02 17:32:32 +0200

    Added quoridor namespace classes extending game namespace classes

---
+++ src/quoridor/QuoridorMove.cs
@@ -0,0 +1,12 @@
+﻿using QuoridorEngine.src.game;
+
+namespace QuoridorEngine.src.quoridor
+{
+    /// <summary>
+    /// Quoridor Move class extending abstract class Move 
+    /// </summary>
+    internal class QuoridorMove : Move
+    {
+
+    }
+}
