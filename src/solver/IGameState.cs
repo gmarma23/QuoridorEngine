f47112883b036bd0447af2c44ae15562c7af60e3
Alex Kostas <k.alexandros299@gmail.com>
2022-11-02 03:23:21 +0200

    Replaced simple comments with XML comments and updated IGameState to better fit the purposes of the solver

--- src/solver/IGameState.cs
+++ src/solver/IGameState.cs
@@ -9,19 +9,37 @@ namespace GameSolver
 	/// </summary>
 	public interface IGameState
 	{
-		// Returns true if the state is terminal (i.e. game is over)
-		public bool IsTerminalState();
+        /// <summary>
+        /// Returns true if the state is terminal (i.e. game is over)
+        /// </summary>
+        /// <returns>True if the state is terminal (i.e. game is over)</returns>
+        public bool IsTerminalState();
 
-		// Returns a list of all the possible moves from this state
-		public List<Move> GetPossibleMoves();
+        /// <summary>
+        /// Returns a list of all the possible moves from this state for a given player
+        /// </summary>
+		/// <param name="playerIsWhite">True if we want moves for white player, false otherwise</param>
+        /// <returns>A list of all the possible moves from this state for given player</returns>
+        public List<Move> GetPossibleMoves(bool playerIsWhite);
 
-		// Executes a given move in this state
-		public void ExecuteMove(Move move);
+        /// <summary>
+        /// Executes a given move in this state
+        /// </summary>
+        /// <param name="move">The move to be executed</param>
+        public void ExecuteMove(Move move);
 
-		// Undoes a given move returning the state to its previous configuration
-		public void UndoMove(Move move);
+        /// <summary>
+        /// Undoes a given move returning the state to its previous configuration. Assumes the move to be
+        /// undone was legal at the moment it was executed.
+        /// </summary>
+        /// <param name="move">The move to be undone</param>
+        public void UndoMove(Move move);
 
-		// Returns an evaluation of the likelyhood of selected player winning the game from this state
-		public float EvaluateState(bool playerIsWhite);
+        /// <summary>
+        /// Returns an evaluation of the likelyhood of selected player winning the game from this state
+        /// </summary>
+        /// <param name="playerIsWhite">True if player we are asking for is white, false otherwise</param>
+        /// <returns>An evaluation of the likelyhood of selected player winning the game from this state</returns>
+        public float EvaluateState(bool playerIsWhite);
 	}
 }
--- src/solver/ISolver.cs
+++ src/solver/ISolver.cs
@@ -7,7 +7,12 @@ namespace GameSolver
 	/// </summary>
 	public interface ISolver
 	{
-		// Returns the best move for the maximizing player given a specific game configuration
-		public Move GetBestMove(IGameState state, bool whiteIsMaximizingPlayer);
+        /// <summary>
+        /// Calculates and returns the best move for the maximizing player given a specific game configuration
+        /// </summary>
+        /// <param name="currentState">The current game configuration</param>
+        /// <param name="whiteIsMaximizingPlayer">True if white is the player asking for a move, false otherwise</param>
+        /// <returns>A move that according to the solving algorithm is the best for the maximizing player</returns>
+        public Move GetBestMove(IGameState currentState, bool whiteIsMaximizingPlayer = true);
 	}
 }
