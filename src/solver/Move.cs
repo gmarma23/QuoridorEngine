10927b8c698e05f1dfb060434356e8d455380661
Alex Kostas <k.alexandros299@gmail.com>
2022-11-02 03:09:02 +0200

    Created basic framework classes

---
+++ src/solver/Move.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Common class to represent a move in any given game.
+/// Any implementation of a particular game needs to extend
+/// (inherit from) this class to provide its particular implementation
+/// details related to the given game.
+/// </summary>
+public class Move
+{
+}
