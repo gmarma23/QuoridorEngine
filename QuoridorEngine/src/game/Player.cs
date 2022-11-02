2811543d215f4090a56a09d80db95dac85287238
gmarma23 <gmarma99@gmail.com>
2022-11-02 16:55:15 +0200

    Performed minor restructures to abstract game core structure and added comments

--- QuoridorEngine/src/game/Player.cs
+++ QuoridorEngine/src/game/Player.cs
@@ -1,13 +1,45 @@
 ﻿namespace QuoridorEngine.src.game
 {
+    /// <summary>
+    /// Common class to represent a piece in any given game.
+    /// Any implementation of a particular game
+    /// needs to extend this class accordingly
+    /// </summary>
+    internal abstract class Piece
+    {
+        protected BoardNode? location;
+
+        public abstract BoardNode? Location { set; get; }
+
+    }
+
+    /// <summary>
+    /// Common class to represent a player in any given game.
+    /// Any implementation of a particular game
+    /// needs to extend this class accordingly
+    /// </summary>
     internal abstract class Player
     {
-        protected bool id;
-        protected int x;
-        protected int y;
+        protected List<Piece>? pieces;
+        protected char id;
 
-        public abstract int X { set; get; }
-        public abstract int Y { set; get; }
-        public abstract bool Id { set; get; }
+        public abstract char Id { set; get; }
+
+        Player()
+        {
+            pieces = new List<Piece>();
+        }
+
+        /// <summary>
+        /// Add new piece to a player
+        /// </summary>
+        /// <param name="piece">Piece to be added</param>
+        public abstract void AddPiece(Piece piece);
+
+        /// <summary>
+        /// Remove piece from a player
+        /// </summary>
+        /// <param name="piece">Piece to be removed</param>
+        public abstract void RemovePiece(Piece piece);
     }
 }
