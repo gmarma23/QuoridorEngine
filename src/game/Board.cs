40213cf5fbb9e54ae4c416c8cb755f7886f03427
gmarma23 <gmarma99@gmail.com>
2022-11-02 16:55:15 +0200

    Performed minor restructures to abstract game core structure and added comments

--- src/game/Board.cs
+++ src/game/Board.cs
@@ -1,8 +1,17 @@
-﻿namespace QuoridorEngine.src.game
+﻿using System.Security.Permissions;
+
+namespace QuoridorEngine.src.game
 {
+    /// <summary>
+    /// Common class to represent a board node in any given board game.
+    /// Any implementation of a particular game
+    /// needs to extend this class accordingly
+    /// </summary>
     internal abstract class BoardNode
-    {
+    {   
+        // List containing all neighbours of particular node
         protected List<BoardNode> neighbours;
+
         protected bool isOccupied;
         protected int x;
         protected int y;
@@ -16,23 +25,35 @@
             neighbours = new List<BoardNode>();
         }
 
-        public void AppendNeighbour(BoardNode node)
-        {
-            neighbours.Add(node);
-        }
+        /// <summary>
+        /// Append new neighbour node to current node based on 
+        /// specific conditions according to a particular game
+        /// </summary>
+        /// <param name="node">Neighbour node</param>
+        public abstract void AppendNeighbour(BoardNode node);
 
-        public void RemoveNeighbour(BoardNode node)
-        {
-            neighbours.Remove(node);
-        }
+        /// <summary>
+        /// Remove neighbour node from current node based on 
+        /// specific conditions according to a particular game
+        /// </summary>
+        /// <param name="node">Neighbour node</param>
+        public abstract void RemoveNeighbour(BoardNode node);
 
+        // Get all neighbour nodes of current node
         public abstract void GetNeighbours();
     }
 
+    /// <summary>
+    /// Common class to represent a board in any given board game.
+    /// Any implementation of a particular game
+    /// needs to extend this class accordingly
+    /// </summary>
     internal abstract class Board
     {
-        protected List<(int, int)> playersPos;
+        // List containing all board nodes.
+        // Behaves like a psudo graph that maps the entire board.
         protected List<BoardNode> boardNodes;
+
         protected int width;
         protected int height;
         
@@ -42,11 +63,18 @@
         Board()
         {
             boardNodes = new List<BoardNode>();
-            playersPos = new List<(int, int)>();
         }
 
+        /// <summary>
+        /// Update board according to newly played moves
+        /// </summary>
+        /// <param name="move">Last played move</param>
         public abstract void Update(Move move);
 
+        // Initialize board at the begining of aech round
+        public abstract void Initialize();
+
+        // Clear board
         public abstract void Clear();
     }
 }
