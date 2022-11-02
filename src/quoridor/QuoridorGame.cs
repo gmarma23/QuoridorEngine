1387c42d1164527057eeac897be3fda124999583
gmarma23 <gmarma99@gmail.com>
2022-11-02 23:21:19 +0200

    Transfered properties from Game namespace classes

---
+++ src/quoridor/QuoridorGame.cs
@@ -0,0 +1,62 @@
+﻿using GameSolver;
+using QuoridorEngine.src.solver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoridorEngine.src.quoridor
+{
+    internal class QuoridorGame : IGameState
+    {
+        /// <summary>
+        /// Returns true if the state is terminal (i.e. game is over)
+        /// </summary>
+        /// <returns>True if the state is terminal (i.e. game is over)</returns>
+        public bool IsTerminalState()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a list of all the possible moves from this state for a given player
+        /// </summary>
+		/// <param name="playerIsWhite">True if we want moves for white player, false otherwise</param>
+        /// <returns>A list of all the possible moves from this state for given player</returns>
+        public List<Move> GetPossibleMoves(bool playerIsWhite)
+        {
+            List<Move> possibleMoves = new List<Move>();
+            return possibleMoves;
+        }
+
+        /// <summary>
+        /// Executes a given move in this state
+        /// </summary>
+        /// <param name="move">The move to be executed</param>
+        public void ExecuteMove(Move move)
+        {
+
+        }
+
+        /// <summary>
+        /// Undoes a given move returning the state to its previous configuration. Assumes the move to be
+        /// undone was legal at the moment it was executed.
+        /// </summary>
+        /// <param name="move">The move to be undone</param>
+        public void UndoMove(Move move)
+        {
+
+        }
+
+        /// <summary>
+        /// Returns an evaluation of the likelyhood of selected player winning the game from this state
+        /// </summary>
+        /// <param name="playerIsWhite">True if player we are asking for is white, false otherwise</param>
+        /// <returns>An evaluation of the likelyhood of selected player winning the game from this state</returns>
+        public float EvaluateState(bool playerIsWhite)
+        {
+            return 0;
+        }
+    }
+}
--- src/quoridor/QuoridorPlayer.cs
+++ src/quoridor/QuoridorPlayer.cs
@@ -21,8 +21,49 @@ namespace QuoridorEngine.src.quoridor
     /// <summary>
     /// Class representing quoridor player
     /// </summary>
-    internal class QuoridorPlayer //: Player
+    internal class QuoridorPlayer
     {
+        private readonly int totalWalls = 10;
+        private int availableWalls;
+        private int victories = 0;
+        private int defeats = 0;
+        private int draws = 0;
+        private bool isWhite;
+        
+        public int AvailableWalls
+        {
+            get { return availableWalls; }
+        }
+
+        public bool IsWhite
+        { 
+            set { isWhite = value; }
+            get { return isWhite; } 
+        }
+
+        public int Victories
+        {
+            set { victories++; }
+            get { return victories; }
+        }
+
+        public int Defeats
+        {
+            set { defeats++; }
+            get { return defeats; }
+        }
+
+        public int Draws
+        {
+            set { draws++; }
+            get { return draws; }
+        }
+
+        QuoridorPlayer(bool isWhite)
+        {
+            availableWalls = totalWalls;
+            IsWhite = isWhite;
+        }
 
     }
 }
