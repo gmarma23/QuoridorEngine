6edf4457b5c7fb7e42df8d759ac46c158c332531
gmarma23 <gmarma99@gmail.com>
2022-11-02 14:24:45 +0200

    Created Game namespace as core structure for board games and added draft implementations of basic classes

---
+++ QuoridorEngine/src/game/Board.cs
@@ -0,0 +1,52 @@
+﻿namespace QuoridorEngine.src.game
+{
+    internal abstract class BoardNode
+    {
+        protected List<BoardNode> neighbours;
+        protected bool isOccupied;
+        protected int x;
+        protected int y;
+
+        public abstract int X { set; get; }
+        public abstract int Y { set; get; }
+        public abstract bool IsOccupied { set; get; }
+
+        BoardNode()
+        {
+            neighbours = new List<BoardNode>();
+        }
+
+        public void AppendNeighbour(BoardNode node)
+        {
+            neighbours.Add(node);
+        }
+
+        public void RemoveNeighbour(BoardNode node)
+        {
+            neighbours.Remove(node);
+        }
+    }
+
+    internal abstract class Board
+    {
+        protected List<(int, int)> playersPos;
+        protected List<BoardNode> boardNodes;
+        protected int width;
+        protected int height;
+        
+        public abstract int Width { set; get; }
+        public abstract int Height { set; get; }
+
+        Board()
+        {
+            boardNodes = new List<BoardNode>();
+            playersPos = new List<(int, int)>();
+        }
+
+        public abstract void Update(Move move);
+
+        public abstract void Generate();
+       
+        public abstract void Clear();
+    }
+}
---
+++ QuoridorEngine/src/game/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace QuoridorEngine.src.game
+{
+    internal abstract class Player
+    {
+        protected bool id;
+        protected int x;
+        protected int y;
+
+        public abstract int X { set; get; }
+        public abstract int Y { set; get; }
+        public abstract bool Id { set; get; }
+    }
+}
