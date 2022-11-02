d8f7935057b766d5331e6a2e5ee05b11ba91d1cf
Alex Kostas <k.alexandros299@gmail.com>
2022-11-02 03:09:02 +0200

    Created basic framework classes

---
+++ QuoridorEngine/src/solver/Solver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+/// <summary>
+/// Common interface to be abided by any Solver class
+/// </summary>
+public interface ISolver
+{
+	// Returns the best move for the maximizing player given a specific game configuration
+	public Move GetBestMove(IGameState state, bool whiteIsMaximizingPlayer);
+}
