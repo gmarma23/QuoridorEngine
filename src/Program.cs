e248a04330917f1bef01d5ecca31485c38352e0e
gmarma23 <gmarma99@gmail.com>
2022-11-03 18:30:33 +0200

    Renamed ui related classes

--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,7 @@ namespace QuoridorEngine
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new GUI());
+            Application.Run(new QuoridorUI());
         }
     }
 }
