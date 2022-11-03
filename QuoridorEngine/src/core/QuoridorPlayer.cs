ae90f0d6e068784f6874e1902e92fe8067d1b1ee
Alex Kostas <k.alexandros299@gmail.com>
2022-11-04 01:21:35 +0200

    Refactor QuoridorPlayer

--- QuoridorEngine/src/core/QuoridorPlayer.cs
+++ QuoridorEngine/src/core/QuoridorPlayer.cs
@@ -1,29 +1,59 @@
-﻿namespace QuoridorEngine.Core
+﻿using System.Diagnostics;
+
+namespace QuoridorEngine.Core
 {
     /// <summary>
-    /// Class representing quoridor player
+    /// This class holds all the information and operations related
+    /// to a Quoridor Player.
     /// </summary>
     internal class QuoridorPlayer
     {
-        private int availableWalls;
+        private int availableWalls, row, column;
 
         private readonly bool isWhite;
-        private int row;
-        private int column;
+        private readonly int targetBaseline;
+
+        /// <summary>
+        /// Initializes a new player with the specified parameters
+        /// </summary>
+        /// <param name="isWhite">True if player is white, false if it is black</param>
+        /// <param name="row">The starting row of the player</param>
+        /// <param name="column">The starting column of the player</param>
+        /// <param name="startingWalls">The starting amount of walls</param>
+        /// <param name="targetBaseline">The row the player needs to reach in order to win</param>
+        public QuoridorPlayer(bool isWhite, int row, int column, int startingWalls, int targetBaseline)
+        {
+            Debug.Assert(row >= 0);
+            Debug.Assert(column >= 0);
+            Debug.Assert(startingWalls >= 0);
+            Debug.Assert(targetBaseline >= 0);
+           
+            this.isWhite = isWhite;
+            this.row = row;
+            this.column = column;
+            this.targetBaseline = targetBaseline;
+            availableWalls = startingWalls;
+        }
+
+        /// <summary>
+        /// Increases the available walls of the player by one
+        /// </summary>
+        public void IncreaseAvailableWalls() { availableWalls++; }
+
+        /// <summary>
+        /// Decreases the available walls of the player by one
+        /// </summary>
+        public void DecreaseAvailableWalls() { availableWalls--; }
+
+        /// <summary>
+        /// Returns whether the player has reached the target row
+        /// required to win.
+        /// </summary>
+        public bool IsInTargetBaseline() { return row == targetBaseline; }
 
         public int AvailableWalls { get; }
         public bool IsWhite { get; }
         public int Row { set; get; }
         public int Column { set; get; }
-
-        public QuoridorPlayer(bool isWhite, int startingAmountOfWalls)
-        {
-            availableWalls = startingAmountOfWalls;
-            this.isWhite = isWhite;
-        }
-
-        public void IncreaseAvailableWalls() { availableWalls++; }
-
-        public void DecreaseAvailableWalls() { availableWalls--; }
     }
 }
