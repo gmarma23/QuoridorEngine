dee51226fa683aafab799d6bcd1968da8cc125b2
Alex Kostas <k.alexandros299@gmail.com>
2022-11-03 21:40:21 +0200

    Simplified all player moves to one class

--- QuoridorEngine/src/core/QuoridorMove.cs
+++ QuoridorEngine/src/core/QuoridorMove.cs
@@ -7,15 +7,43 @@ namespace QuoridorEngine.Core
     /// </summary>
     internal class QuoridorMove : Move
     {
-        private int row, column;
+        private readonly int row, column;
+        private readonly bool isWhitePlayer;
+        private readonly Orientation orientation;
+        private readonly MoveType type;
 
-        public QuoridorMove(int row, int column)
+        public QuoridorMove(int row, int column, bool isWhitePlayer, Orientation orientation)
         {
             this.row = row;
             this.column = column;
+            this.isWhitePlayer = isWhitePlayer;
+            this.orientation = orientation;
+            type = MoveType.WallPlacement;
+        }
+
+        public QuoridorMove(int row, int column, bool isWhitePlayer)
+        {
+            this.row = row;
+            this.column = column;
+            this.isWhitePlayer = isWhitePlayer;
+            type = MoveType.PlayerMovement;
         }
 
         public int Row { get; }
         public int Column { get; }
+
+        public bool IsWhitePlayer { get; } 
+        public Orientation Orientation { get; } 
+        public MoveType Type { get; }
+    }
+
+    public enum Orientation
+    {
+        Horizontal, Vertical
+    };
+
+    public enum MoveType
+    {
+        WallPlacement, PlayerMovement
     }
 }
