417a593e2a94d95c4ee7d1aa408044a92c97f6bd
Alex Kostas <k.alexandros299@gmail.com>
2022-11-03 21:40:21 +0200

    Simplified all player moves to one class

--- src/core/PlayerMovement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QuoridorEngine.Core
-{
-    internal class PlayerMovement : QuoridorMove
-    {
-        public PlayerMovement(int row, int column) : base(row, column)
-        {
-        }
-    }
-}
