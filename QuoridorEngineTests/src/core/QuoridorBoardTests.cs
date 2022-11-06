76fe053e8ffc6b41bb500d6e9bf46d79481ace4f
gmarma23 <gmarma99@gmail.com>
2022-11-06 13:29:59 +0200

    Added unit tests for all QuoridorBoard class methods

--- QuoridorEngineTests/src/core/QuoridorBoardTests.cs
+++ QuoridorEngineTests/src/core/QuoridorBoardTests.cs
@@ -1,5 +1,7 @@
 ﻿using QuoridorEngine.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System.Security.Policy;
 
 namespace QuoridorEngine.Core.Tests
 {
@@ -11,12 +13,12 @@ namespace QuoridorEngine.Core.Tests
         [DataRow(3)]
         [DataRow(201)]
         [DataRow(35)]
-        [DataRow(7)]
 
-        public void QuoridorBoardTest(int dimension)
+        public void QuoridorBoardConstructorTest(int dimension)
         {
             QuoridorBoard board = new QuoridorBoard(dimension);
             Assert.IsNotNull(board);
+
         }
 
         [TestMethod()]
@@ -61,20 +63,40 @@ namespace QuoridorEngine.Core.Tests
         }
 
         [TestMethod()]
+        [DataRow(3, 1, 0)]
+        [DataRow(3, 1, 1)]
+        [DataRow(3, 1, 2)]
+        [DataRow(3, 2, 0)]
+        [DataRow(3, 2, 1)]
+        [DataRow(3, 2, 2)]
+
+        [DataRow(5, 1, 0)]
+        [DataRow(5, 1, 1)]
+        [DataRow(5, 1, 3)]
+        [DataRow(5, 3, 0)]
+        [DataRow(5, 4, 1)]
+        [DataRow(5, 4, 4)]
+
+        [DataRow(6, 1, 0)]
+        [DataRow(6, 1, 1)]
+        [DataRow(6, 1, 2)]
+        [DataRow(6, 4, 0)]
+        [DataRow(6, 5, 4)]
+        [DataRow(6, 5, 5)]
+
         [DataRow(9, 1, 0)]
         [DataRow(9, 1, 1)]
-        [DataRow(9, 2, 3)]
-        [DataRow(9, 5, 6)]
-        [DataRow(9, 7, 8)]
-        [DataRow(9, 7, 8)]
+        [DataRow(9, 1, 3)]
+        [DataRow(9, 3, 0)]
+        [DataRow(9, 5, 2)]
+        [DataRow(9, 7, 6)]
+        [DataRow(9, 8, 0)]
+        [DataRow(9, 8, 1)]
+        [DataRow(9, 8, 2)]
+        [DataRow(9, 8, 4)]
+        [DataRow(9, 8, 7)]
+        [DataRow(9, 8, 8)]
 
-        [DataRow(3, 1, 0)]
-        [DataRow(3, 2, 2)]
-        [DataRow(3, 2, 3)]
-        [DataRow(3, 1, 1)]
-        [DataRow(3, 3, 2)]
-
-        [DataRow(4, 4, 4)]
         public void AddWallPartHorizontalTest(int dimension, int row, int col)
         {
             QuoridorBoard board = new QuoridorBoard(dimension);
@@ -83,6 +105,295 @@ namespace QuoridorEngine.Core.Tests
             Assert.IsTrue(board.CheckWallPartHorizontal(row, col));
         }
 
-        
+        [TestMethod()]
+        [DataRow(3, 1, 0)]
+        [DataRow(3, 1, 1)]
+        [DataRow(3, 1, 2)]
+        [DataRow(3, 2, 0)]
+        [DataRow(3, 2, 1)]
+        [DataRow(3, 2, 2)]
+
+        [DataRow(5, 1, 0)]
+        [DataRow(5, 1, 1)]
+        [DataRow(5, 1, 3)]
+        [DataRow(5, 3, 0)]
+        [DataRow(5, 4, 1)]
+        [DataRow(5, 4, 4)]
+
+        [DataRow(6, 1, 0)]
+        [DataRow(6, 1, 1)]
+        [DataRow(6, 1, 2)]
+        [DataRow(6, 4, 0)]
+        [DataRow(6, 5, 4)]
+        [DataRow(6, 5, 5)]
+
+        [DataRow(9, 1, 0)]
+        [DataRow(9, 1, 1)]
+        [DataRow(9, 1, 3)]
+        [DataRow(9, 3, 0)]
+        [DataRow(9, 5, 2)]
+        [DataRow(9, 7, 6)]
+        [DataRow(9, 8, 0)]
+        [DataRow(9, 8, 1)]
+        [DataRow(9, 8, 2)]
+        [DataRow(9, 8, 4)]
+        [DataRow(9, 8, 7)]
+        [DataRow(9, 8, 8)]
+
+        public void RemoveWallPartHorizontalTest(int dimension, int row, int col)
+        {
+            QuoridorBoard board = new QuoridorBoard(dimension);
+            board.AddWallPartHorizontal(row, col);
+            board.RemoveWallPartHorizontal(row, col);
+
+            Assert.IsFalse(board.CheckWallPartHorizontal(row, col));
+        }
+
+        [TestMethod()]
+        [DataRow(3, 0, 1)]
+        [DataRow(3, 0, 2)]
+        [DataRow(3, 1, 1)]
+        [DataRow(3, 1, 2)]
+        [DataRow(3, 2, 1)]
+        [DataRow(3, 2, 2)]
+
+        [DataRow(5, 0, 1)]
+        [DataRow(5, 0, 3)]
+        [DataRow(5, 1, 1)]
+        [DataRow(5, 1, 4)]
+        [DataRow(5, 3, 1)]
+        [DataRow(5, 4, 4)]
+
+        [DataRow(6, 0, 1)]
+        [DataRow(6, 0, 4)]
+        [DataRow(6, 1, 1)]
+        [DataRow(6, 2, 1)]
+        [DataRow(6, 4, 5)]
+        [DataRow(6, 5, 5)]
+
+        [DataRow(9, 0, 1)]
+        [DataRow(9, 1, 1)]
+        [DataRow(9, 3, 1)]
+        [DataRow(9, 0, 3)]
+        [DataRow(9, 2, 5)]
+        [DataRow(9, 6, 7)]
+        [DataRow(9, 0, 8)]
+        [DataRow(9, 1, 8)]
+        [DataRow(9, 2, 8)]
+        [DataRow(9, 4, 8)]
+        [DataRow(9, 7, 8)]
+        [DataRow(9, 8, 8)]
+
+        public void CheckWallPartVerticalTest(int dimension, int row, int col)
+        {
+            QuoridorBoard board = new QuoridorBoard(dimension);
+            Assert.IsFalse(board.CheckWallPartVertical(row, col));
+        }
+
+        [TestMethod()]
+        [DataRow(3, 0, 1)]
+        [DataRow(3, 0, 2)]
+        [DataRow(3, 1, 1)]
+        [DataRow(3, 1, 2)]
+        [DataRow(3, 2, 1)]
+        [DataRow(3, 2, 2)]
+
+        [DataRow(5, 0, 1)]
+        [DataRow(5, 0, 3)]
+        [DataRow(5, 1, 1)]
+        [DataRow(5, 1, 4)]
+        [DataRow(5, 3, 1)]
+        [DataRow(5, 4, 4)]
+
+        [DataRow(6, 0, 1)]
+        [DataRow(6, 0, 4)]
+        [DataRow(6, 1, 1)]
+        [DataRow(6, 2, 1)]
+        [DataRow(6, 4, 5)]
+        [DataRow(6, 5, 5)]
+
+        [DataRow(9, 0, 1)]
+        [DataRow(9, 1, 1)]
+        [DataRow(9, 3, 1)]
+        [DataRow(9, 0, 3)]
+        [DataRow(9, 2, 5)]
+        [DataRow(9, 6, 7)]
+        [DataRow(9, 0, 8)]
+        [DataRow(9, 1, 8)]
+        [DataRow(9, 2, 8)]
+        [DataRow(9, 4, 8)]
+        [DataRow(9, 7, 8)]
+        [DataRow(9, 8, 8)]
+
+        public void AddWallPartVerticalTest(int dimension, int row, int col)
+        {
+            QuoridorBoard board = new QuoridorBoard(dimension);
+            board.AddWallPartVertical(row, col);
+
+            Assert.IsTrue(board.CheckWallPartVertical(row, col));
+        }
+
+        [TestMethod()]
+        [DataRow(3, 0, 1)]
+        [DataRow(3, 0, 2)]
+        [DataRow(3, 1, 1)]
+        [DataRow(3, 1, 2)]
+        [DataRow(3, 2, 1)]
+        [DataRow(3, 2, 2)]
+
+        [DataRow(5, 0, 1)]
+        [DataRow(5, 0, 3)]
+        [DataRow(5, 1, 1)]
+        [DataRow(5, 1, 4)]
+        [DataRow(5, 3, 1)]
+        [DataRow(5, 4, 4)]
+
+        [DataRow(6, 0, 1)]
+        [DataRow(6, 0, 4)]
+        [DataRow(6, 1, 1)]
+        [DataRow(6, 2, 1)]
+        [DataRow(6, 4, 5)]
+        [DataRow(6, 5, 5)]
+
+        [DataRow(9, 0, 1)]
+        [DataRow(9, 1, 1)]
+        [DataRow(9, 3, 1)]
+        [DataRow(9, 0, 3)]
+        [DataRow(9, 2, 5)]
+        [DataRow(9, 6, 7)]
+        [DataRow(9, 0, 8)]
+        [DataRow(9, 1, 8)]
+        [DataRow(9, 2, 8)]
+        [DataRow(9, 4, 8)]
+        [DataRow(9, 7, 8)]
+        [DataRow(9, 8, 8)]
+
+        public void RemoveWallPartVerticalTest(int dimension, int row, int col)
+        {
+            QuoridorBoard board = new QuoridorBoard(dimension);
+            board.AddWallPartVertical(row, col);
+            board.RemoveWallPartVertical(row, col);
+
+            Assert.IsFalse(board.CheckWallPartVertical(row, col));
+        }
+
+        [TestMethod()]
+        [DataRow(3, 1, 1)]
+        [DataRow(3, 1, 2)]
+        [DataRow(3, 2, 1)]
+        [DataRow(3, 2, 2)]
+
+        [DataRow(5, 1, 1)]
+        [DataRow(5, 1, 2)]
+        [DataRow(5, 2, 1)]
+        [DataRow(5, 3, 2)]
+        [DataRow(5, 3, 3)]
+        [DataRow(5, 4, 4)]
+
+        [DataRow(6, 1, 1)]
+        [DataRow(6, 3, 4)]
+        [DataRow(6, 4, 4)]
+        [DataRow(6, 5, 1)]
+        [DataRow(6, 5, 3)]
+        [DataRow(6, 5, 5)]
+
+        [DataRow(9, 1, 1)]
+        [DataRow(9, 3, 1)]
+        [DataRow(9, 4, 3)]
+        [DataRow(9, 5, 5)]
+        [DataRow(9, 5, 7)]
+        [DataRow(9, 5, 8)]
+        [DataRow(9, 6, 1)]
+        [DataRow(9, 7, 6)]
+        [DataRow(9, 7, 7)]
+        [DataRow(9, 7, 8)]
+        [DataRow(9, 8, 8)]
+
+        public void CheckCornerTest(int dimension, int row, int col)
+        {
+            QuoridorBoard board = new QuoridorBoard(dimension);
+            Assert.IsFalse(board.CheckCorner(row, col));
+        }
+
+        [TestMethod()]
+        [DataRow(3, 1, 1)]
+        [DataRow(3, 1, 2)]
+        [DataRow(3, 2, 1)]
+        [DataRow(3, 2, 2)]
+
+        [DataRow(5, 1, 1)]
+        [DataRow(5, 1, 2)]
+        [DataRow(5, 2, 1)]
+        [DataRow(5, 3, 2)]
+        [DataRow(5, 3, 3)]
+        [DataRow(5, 4, 4)]
+
+        [DataRow(6, 1, 1)]
+        [DataRow(6, 3, 4)]
+        [DataRow(6, 4, 4)]
+        [DataRow(6, 5, 1)]
+        [DataRow(6, 5, 3)]
+        [DataRow(6, 5, 5)]
+
+        [DataRow(9, 1, 1)]
+        [DataRow(9, 3, 1)]
+        [DataRow(9, 4, 3)]
+        [DataRow(9, 5, 5)]
+        [DataRow(9, 5, 7)]
+        [DataRow(9, 5, 8)]
+        [DataRow(9, 6, 1)]
+        [DataRow(9, 7, 6)]
+        [DataRow(9, 7, 7)]
+        [DataRow(9, 7, 8)]
+        [DataRow(9, 8, 8)]
+
+        public void AddCornerTest(int dimension, int row, int col)
+        {
+            QuoridorBoard board = new QuoridorBoard(dimension);
+            board.AddCorner(row, col);
+
+            Assert.IsTrue(board.CheckCorner(row, col));
+        }
+
+        [TestMethod()]
+        [DataRow(3, 1, 1)]
+        [DataRow(3, 1, 2)]
+        [DataRow(3, 2, 1)]
+        [DataRow(3, 2, 2)]
+
+        [DataRow(5, 1, 1)]
+        [DataRow(5, 1, 2)]
+        [DataRow(5, 2, 1)]
+        [DataRow(5, 3, 2)]
+        [DataRow(5, 3, 3)]
+        [DataRow(5, 4, 4)]
+
+        [DataRow(6, 1, 1)]
+        [DataRow(6, 3, 4)]
+        [DataRow(6, 4, 4)]
+        [DataRow(6, 5, 1)]
+        [DataRow(6, 5, 3)]
+        [DataRow(6, 5, 5)]
+
+        [DataRow(9, 1, 1)]
+        [DataRow(9, 3, 1)]
+        [DataRow(9, 4, 3)]
+        [DataRow(9, 5, 5)]
+        [DataRow(9, 5, 7)]
+        [DataRow(9, 5, 8)]
+        [DataRow(9, 6, 1)]
+        [DataRow(9, 7, 6)]
+        [DataRow(9, 7, 7)]
+        [DataRow(9, 7, 8)]
+        [DataRow(9, 8, 8)]
+
+        public void RemoveCornerTest(int dimension, int row, int col)
+        {
+            QuoridorBoard board = new QuoridorBoard(dimension);
+            board.AddCorner(row, col);
+            board.RemoveCorner(row, col);
+
+            Assert.IsFalse(board.CheckCorner(row, col));
+        }
     }
 }
