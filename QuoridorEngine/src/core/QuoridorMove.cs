6fbf813597644f4b427f66e54d8ebc3cac254f2b
Alex Kostas <k.alexandros299@gmail.com>
2022-11-06 22:19:40 +0200

    Merge unit-test-game into master

--- QuoridorEngine/src/core/QuoridorMove.cs
+++ QuoridorEngine/src/core/QuoridorMove.cs
@@ -10,7 +10,7 @@ namespace QuoridorEngine.Core
     /// to contain any functionality. Its purpose is to carry all
     /// the move-related data around.
     /// </summary>
-    internal class QuoridorMove : Move
+    public class QuoridorMove : Move
     {
         private readonly int row, column;
         private readonly bool isWhitePlayer;
@@ -26,8 +26,6 @@ namespace QuoridorEngine.Core
         /// <param name="orientation">The orientation of wall (Horizontal/Vertical)</param>
         public QuoridorMove(int row, int column, bool isWhitePlayer, Orientation orientation)
         {
-            Debug.Assert(row >= 0);
-            Debug.Assert(column >= 0);
             Debug.Assert(orientation == Orientation.Horizontal || orientation == Orientation.Vertical);
 
             this.row = row;
@@ -45,20 +43,17 @@ namespace QuoridorEngine.Core
         /// <param name="isWhitePlayer">Whether this move is executed by the white player or not</param>
         public QuoridorMove(int row, int column, bool isWhitePlayer)
         {
-            Debug.Assert(row >= 0);
-            Debug.Assert(column >= 0);
-
             this.row = row;
             this.column = column;
             this.isWhitePlayer = isWhitePlayer;
             type = MoveType.PlayerMovement;
         }
 
-        public int Row { get; }
-        public int Column { get; }
-        public bool IsWhitePlayer { get; } 
-        public Orientation Orientation { get; } 
-        public MoveType Type { get; }
+        public int Row { get => row; }
+        public int Column { get => column; }
+        public bool IsWhitePlayer { get => isWhitePlayer; } 
+        public Orientation Orientation { get => orientation; } 
+        public MoveType Type { get => type; }
     }
 
     /// <summary>
