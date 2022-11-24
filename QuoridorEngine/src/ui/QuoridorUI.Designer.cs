e5cd245858b79fdfd1c5b84569525864a6095638
gmarma23 <gmarma99@gmail.com>
2022-11-24 13:33:48 +0200

    Added main settings for gui frame

--- QuoridorEngine/src/ui/QuoridorUI.Designer.cs
+++ QuoridorEngine/src/ui/QuoridorUI.Designer.cs
@@ -29,10 +29,20 @@ namespace QuoridorEngine.UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // QuoridorUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(974, 652);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "QuoridorUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quoridor Engine";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
