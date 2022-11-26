9e86e55a5cea8fd502b35f6993e7220b740f85b3
gmarma23 <gmarma99@gmail.com>
2022-11-26 17:21:08 +0200

    Introduced BoardCell class to represent board cells in gui

--- QuoridorEngine/src/ui/gui/BoardCell.cs
+++ QuoridorEngine/src/ui/gui/BoardCell.cs
@@ -8,6 +8,23 @@ namespace QuoridorEngine.src.ui.gui
 {
     public class BoardCell : Label
     {
+        private BoardCellType type;
+        private int row;
+        private int column;
 
+        public BoardCellType Type { get => type; }
+        public int Row { get => row; }
+        public int Column { get => column; }
+
+        public BoardCell()
+        {
+
+        }
+    }
+
+    public enum BoardCellType
+    {
+        Player,
+        Wall
     }
 }
