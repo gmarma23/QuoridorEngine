b5b4244c1b6354ffd27612db09cda1693a9c69ab
gmarma23 <gmarma99@gmail.com>
2022-12-13 17:11:58 +0200

    Added application icon

--- QuoridorEngine/resources/Resources.Designer.cs
+++ QuoridorEngine/resources/Resources.Designer.cs
@@ -70,6 +70,16 @@ namespace QuoridorEngine.resources {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon quoridor {
+            get {
+                object obj = ResourceManager.GetObject("quoridor", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream wall_move {
