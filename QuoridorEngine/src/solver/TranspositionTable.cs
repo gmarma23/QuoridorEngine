eada83b59666c27f620d89dce2a51d6cf4470733
gmarma23 <gmarma99@gmail.com>
2023-07-14 16:35:51 +0300

    Fixed Transposition Table's implementation and moved class to separate file

---
+++ QuoridorEngine/src/solver/TranspositionTable.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+namespace QuoridorEngine.Solver
+{
+    public class TranspositionTable
+    {
+        private struct EntryType
+        {
+            public bool Valid { get; set; }
+            public float Evaluation { get; set; }
+
+            public EntryType()
+            {
+                Valid = false;
+                Evaluation = 0;
+            }
+        }
+
+        private readonly EntryType[] table;
+
+        public int Capacity { get; private init; }
+        public int Count { get; private set; }
+
+        public TranspositionTable(int capacity)
+        {
+            if (capacity <= 0)
+                throw new ArgumentException("Invalid Transposition Table capacity!");
+
+            table = new EntryType[capacity];
+            Capacity = capacity;
+
+            Clear();
+        }
+
+        public void Add(long key, float evaluation)
+        {
+            int hashedKey = hash(key);
+            table[hashedKey].Evaluation = evaluation;
+            table[hashedKey].Valid = true;
+            Count++;
+        }
+
+        public float Get(long key)
+        {
+            Debug.Assert(HasKey(key));
+            return table[hash(key)].Evaluation;
+        }
+
+        public bool HasKey(long key)
+        {
+            int hashedKey = hash(key);
+            return table[hashedKey].Valid;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < Capacity; i++)
+                table[i].Valid = false;
+            Count = 0;
+        }
+
+        private int hash(long key) => (int)(key % Capacity);
+    }
+}
